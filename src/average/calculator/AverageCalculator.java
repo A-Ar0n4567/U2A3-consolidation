3009d450498c4a8c66c9af11a160dd544e461ee4
aamir7110 <aamir7110@7LLN6Y1.Student.UGDSB.ED>
2018-10-30 09:24:56 -0400

    U2A3 consolidation

---
+++ src/average/calculator/AverageCalculator.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package average.calculator;
+import java.util.Scanner;
+/**
+ *
+ * @author aamir7110
+ */
+public class AverageCalculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner keyedInput = new Scanner(System.in);
+        
+        double [] marks = new double[5];
+        
+        double average;
+        
+        System.out.println("Enter Your Last Five Marks");
+        
+        for(int i = 0;i<=4; i++){
+            marks[i] = keyedInput.nextDouble();
+        }
+        average = marks[0]+marks[1]+marks[2]+marks[3]+marks[4];
+        average = average/5;
+        average = average*10;
+        average = Math.round(average);
+        average = average/10;
+        System.out.println("Your average is "+average+"%");
+        
+        System.out.println("Here Are Your Marks Ordered From Lowest To Highest:");
+        
+        for(int i = 0;i<=100;i++){
+            
+            if(marks[0] ==i){
+                System.out.println(marks[0]);
+            }
+            if(marks[1] ==i){
+                System.out.println(marks[1]);
+            }
+            if(marks[2] ==i){
+                System.out.println(marks[2]);
+            }
+            if(marks[3] ==i){
+                System.out.println(marks[3]);
+            }
+            if(marks[4] ==i){
+                System.out.println(marks[4]);
+            }
+            
+        }
+        
+    }
+    
+}
