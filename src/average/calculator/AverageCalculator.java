0da1ed1bd74a96cf6663d4c2febf56f5426c1d25
aamir7110 <aamir7110@7LLN6Y1.Student.UGDSB.ED>
2018-10-31 09:39:53 -0400

    U2A3 consolidation

--- src/average/calculator/AverageCalculator.java
+++ src/average/calculator/AverageCalculator.java
@@ -18,25 +18,27 @@ public class AverageCalculator {
     public static void main(String[] args) {
         // TODO code application logic here
         Scanner keyedInput = new Scanner(System.in);
-        
+        //variable declaration
         double [] marks = new double[5];
         
         double average;
-        
+        //asks for 5 numbers
         System.out.println("Enter Your Last Five Marks");
-        
+        //repeats keyed input 5 times
         for(int i = 0;i<=4; i++){
             marks[i] = keyedInput.nextDouble();
         }
+        //calculates Average
         average = marks[0]+marks[1]+marks[2]+marks[3]+marks[4];
         average = average/5;
         average = average*10;
         average = Math.round(average);
         average = average/10;
+       //displays average
         System.out.println("Your average is "+average+"%");
         
         System.out.println("Here Are Your Marks Ordered From Lowest To Highest:");
-        
+        //orders marks from lowest to highest 
         for(int i = 0;i<=100;i++){
             
             if(marks[0] ==i){
